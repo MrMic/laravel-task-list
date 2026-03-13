e140d4695d0ff4cfc1c5426cbf51108c048ad41f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-03-13 12:33:29 +0100

    feat(routes): implement basic routes, redirects, and fallback - 05/
       - Update root route to return a "Main Page" string for testing
       - Add named '/hello' route and a redirect from '/hallo'
       - Add dynamic routing with the '/hello/{name}' parameter
       - Implement a global fallback route to handle 404 errors gracefully

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
+// ______________________________________________________________________
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Main Page';
+});
+
+// ______________________________________________________________________
+Route::get('/hello', function () {
+    return 'Hello World';
+})->name('hello');
+
+// ______________________________________________________________________
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+// ______________________________________________________________________
+Route::get('/hello/{name}', function ($name) {
+    return 'Hello ' . $name;
+});
+
+// ______________________________________________________________________
+Route::fallback(function () {
+    return '404 Not Found';
 });
